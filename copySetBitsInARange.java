f7b0a998b99480f5f03cc9ea399801a0616e4ef2
Abhay <chauhan27.abhay@gmail.com>
2020-11-16 15:34:55 +0530

    copy set bits in a range

---
+++ copySetBitsInARange.java
@@ -0,0 +1,19 @@
+class copySetBitsInARange{
+
+    public static void main(String[] args){
+        int a = 10;
+        int b = 13;
+        int left =2;
+        int right = 3;
+
+        int mask = (1 << (right-left+1));
+        mask--;
+        mask = (1<<(left-1));
+
+        mask = (a & (mask));
+
+        b = b | mask;
+
+        System.out.println(b);
+    }
+}
